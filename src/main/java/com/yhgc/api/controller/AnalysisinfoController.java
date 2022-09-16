01757c1f3ee08bbd0f439dd3cab3222bf96f1a7b
yishengxiong <2263301835@qq.com>
2022-09-16 10:36:58 +0800

    修改

--- src/main/java/com/yhgc/api/controller/AnalysisinfoController.java
+++ src/main/java/com/yhgc/api/controller/AnalysisinfoController.java
@@ -10,9 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
